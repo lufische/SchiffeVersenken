bfb0de1399bc54c9c36dc75085b9a5c39f41f048
Lukas Fischer <phy87736@stud.uni-stuttgart.de>
2016-05-17 18:00:22 +0200

    Update

--- client.py
+++ client.py
@@ -1,22 +1,155 @@
 #!/usr/bin/python           # This is client.py file
-
 import socket               # Import socket module
 import numpy as np
+import time
 
-s = socket.socket()         # Create a socket object
+
+debug = True
+
+################################################################################
+###                        HELPER FUNCTIONS                                  ###
+################################################################################
+def estConnection(host, port):
+  """Establishes connection to the server
+  Input:
+    host:     Host Adress
+    port:     Port for connection
+  Return:
+    int success: 1 if successfull"""
+  sock = socket.socket()         # Create a socket object
+  sock.connect((host, port))
+  buff = sock.recv(1024)          # Handshake positive
+  if (buff[0] == 'Y'):
+    # Handshake positive
+    print(buff[1:])
+    return 1, sock
+  else:
+    return 0, None
+    
+    
+    
+def saveSend(sock, command):
+  """Sends the command to the server and checks if the command
+  was submitted correctly
+  Inout:
+    command:     string
+  Return:
+    int          successfull. Raises error if not."""
+  sent = sock.send(command)
+  for i in range(10):
+    if (sent < len(command)) and debug:
+      print('ERROR> During sending - Retry')
+    else:      
+      time.sleep(0.0005)
+      return 1
+  if (sent == 0):
+    raise "ERROR> No communication with server possible"
+    return 0
+    
+    
+    
+    
+def mapRequest(sock, x, y):
+  """Sends map request and parses return
+  Input:
+    x:    Row position
+    y:    Col position
+  Return:
+    int   tile state at given position. The value corresponds to the 
+          state as follows:
+          0 - Unknown
+          2 - Allready bombed in a previous round
+          3 - Successfull hit in a previous round"""
+  # =============================== SEND COMMAND ================================
+  res = saveSend(sock, "M, {}, {}".format(x, y))
+
+  # =============================== PARSE INPUT ================================+
+  # time.sleep(0.01)
+  buff = sock.recv(1024)        # Get Result
+  splittedBuff = buff.split(',')
+  if   splittedBuff[0] == 'I': print('ERROR> Invalid Command')
+  elif splittedBuff[0] == 'R':
+    try:    res = int(splittedBuff[1])
+    except:
+      raise "Error> Server returned non int result on Map request"
+      return -1
+    else:
+      return res
+  elif splittedBuff[0] == 'T' or splittedBuff[0] == 'N':
+    print("Turn not initialated")
+    return -1
+  else:
+    raise "ERROR> Unexpected server string"
+      
+      
+      
+      
+def bomb(sock, x, y):
+  """Sends bomb request and parses return
+  Input:
+    x:    Row position
+    y:    Col position
+  Return:
+    int   Hit (1) or not hit (0)
+    int   Ship destroyed (1) or not (0)"""
+  # =============================== SEND COMMAND ================================
+  res = saveSend(sock, "B, {}, {}".format(x, y))
+
+  # =============================== PARSE INPUT ================================+
+  buff = sock.recv(1024)        # Get Result
+  splittedBuff = buff.split(',')
+  if   splittedBuff[0] == 'I': print('ERROR> Invalid Command')
+  elif splittedBuff[0] == 'R':
+    try:    
+      res = int(splittedBuff[1])
+      dest = int(splittedBuff[1])
+    except:
+      raise "Error> Server returned non int result on bomb request"
+      return -1, -1
+    else:
+      return res, dest
+  elif splittedBuff[0] == 'T' or splittedBuff[0] == 'N':
+    print("Turn not initialated")
+    return -1, -1
+  else:
+    raise "ERROR> Unexpected server string"
+    
+
+################################################################################
+###                                MAIN                                      ###
+################################################################################
+
+# ======================== CONNECT TO SERVER ===================================
 host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
+res, sock = estConnection(host, port)
+time.sleep(0.0005)
+if (res == 0):
+  raise "ERROR> Connection could not be established"
 
-s.connect((host, port))
-print s.recv(1024)          # Handshake positive
 
-while True:
-  buff = s.recv(1024)        # Start Turn
+# ============================ PLAY ROUND ======================================
+isEOG = False
+while not(isEOG):
+  buff = sock.recv(1024)        # Start Turn
   if (buff == 'T'):
-    s.send("B, {}, {}".format(np.random.randint(10), np.random.randint(10)))
-    buff = s.recv(1024)        # Result
-    print(buff)
-  if (buff == "EOG"): break
-  
+    res = saveSend(sock, 'Y')
+    if (debug):
+      print('====TURN===')
+    for i in range(1000):
+      target = [np.random.randint(10), np.random.randint(10)]
+      res = mapRequest(sock, target[0], target[1])
+      if (res == 0):    
+        hit, dest = bomb(sock, target[0], target[1])
+        if (debug):
+          print("Bombed - Hit: {}, dest: {}".format(hit, dest))
+        hit, dest = bomb(sock, 2, 2)
+        if (debug):
+          print("Bombed - Hit: {}, dest: {}".format(hit, dest))
+        break
+  elif (buff == "EOG"):
+    isEOG = True
+  elif (buff[0] == "N"):
+    print("Unsynced client behaviour")
 
-s.close                     # Close the socket when done
+sock.close                     # Close the socket when done
--- server.py
+++ server.py
@@ -4,158 +4,320 @@ import socket               # Import socket module
 import numpy as np
 import matplotlib.pyplot as pp
 import string as st
+import time
+
+debug = True
+visu  = False
+
 
 ################################################################################
 ###                        HELPER FUNCTIONS                                  ###
 ################################################################################
-def setShipsRandom(shipMap, pl):
-  # Note: The array is given as reference here and not as a copy
-  for shipLen in [5, 4, 4, 3, 3, 3, 2, 2, 2, 2]:
-    shipsSet = False
-    while not(shipsSet):
-      valid = False
+def saveSend(sock, command):
+  """Sends the command to the server and checks if the command
+  was submitted correctly
+  Inout:
+    command:     string
+  Return:
+    int          successfull. Raises error if not."""
+  sent = sock.send(command)
+  for i in range(10):
+    if (sent < len(command)) and debug:
+      print('ERROR> During sending - Retry')
+    else:
+      time.sleep(0.0005)
+      return 1
+  if (sent == 0):
+    raise "ERROR> No communication with server possible"
+    return 0
+    
+
+
+
+
+def setShipsRandom(shipMap):
+  """Place ships randomly on the map. The implemented rule ensures, that ships
+  can be placed on the border, but are not allowed to touch (neither
+  directly nor diagonally).
+  All in all this function places
+    1 Aircraft carrier (length 5)
+    2 Battleships (length 4)
+    3 Destroyer (lenght 3)
+    4 Submarines (lenght 2)
+  Input: 
+    shipMap:    numpy.array (as reference, standard) specifying the ship
+                positions.
+    pl:         Player index
+  """
+  # Note: The shipMap array is given as reference here and not as a copy
+  #       Dependend on the position of the larger ships, it is possible
+  #       that no space for the smaller ships is left. Therefore we just
+  #       start all over again if such a case occurs.
+  # Developper Note: @MU suggested, that it is more efficient to call the
+  #                  ship placement recursively.
+  
+  # ================================ INIT ======================================
+  maxResetTries   = 1000 # Max Number of attempts for whole configuration
+  maxShipSetSteps = 1000 # Max Number of tries to set ship
+  
+  # ============================== SET SHIPS ===================================
+  resetSteps = 0
+  shipsSet = False # Flag: Are all ships correctly set?
+  while not(shipsSet) and (resetSteps<1000):
+    # Start settin the ships from large to small
+    for shipLen in [5, 4, 4, 3, 3, 3, 2, 2, 2, 2]:
+      valid    = False # Flag: Is the current position for one ship valid?
       retrySteps = 0
-      while not(valid) and (retrySteps<100):
+      # Randomly draw position and orientation for current ship until it
+      # is valid
+      while not(valid) and (retrySteps<1000):
         retrySteps += 1
         coord  = np.random.randint(10, size=2)
         orient = np.random.randint( 2, size=1)[0]
+        # Check if ship fits in the game field
         if (coord[orient] + shipLen < 10): 
           valid = True
+        # Check if ship touches other ship
         selVert = [max(coord[0]-1, 0), min(coord[0]+(orient==0)*(shipLen-1)+2, 10)]
         selHorz = [max(coord[1]-1, 0), min(coord[1]+(orient==1)*(shipLen-1)+2, 10)]
-        if shipMap[selVert[0]:selVert[1], selHorz[0]:selHorz[1], pl].any(): 
-          valid = False 
+        if shipMap[selVert[0]:selVert[1], selHorz[0]:selHorz[1]].any(): 
+          valid = False
+      # end: Ship valid
       
       if valid:
+        # Current ship could be correctly placed
         shipMap[coord[0]:coord[0]+(orient==0)*(shipLen-1)+1, 
-                coord[1]:coord[1]+(orient==1)*(shipLen-1)+1, pl] = 1
+                coord[1]:coord[1]+(orient==1)*(shipLen-1)+1] = 1
         shipsSet = True
-        
+      else:
+        # Not possible to set current ship in given number of tries
+        shipsSet = False
+        shipMap[:,:] = 0
+        resetSteps += 1
+        break
 
-def checkDestroyed(shipMap, pl, xy):
-  selPl = (pl+1)%2
+  # ================================ CHECK =====================================
+  if not(shipsSet) and (resetSteps == maxResetTries):
+    raise "ERROR> Ships could not be set"
+  else:
+    return 1
+  # ============================================================================
+
+
+
+def checkDestroyed(shipMap, xy):
+  """Checks if bombing the position xy results in an ship to be sunk.
+  Input:
+    shipMap:    numpy.array (as reference, standard) specifying the state of
+                each tile in a binary fassion:
+                  0: Nothing
+                  1: Undestroyed part of ship
+                  2: Bombed water
+                  3: Destroyed ship
+    xy:         list or numpy.array giving [Row, Column] of attack
+  """
+  if (shipMap[xy[0], xy[1]]%2 == 0):
+    # No hit at all
+    return 0
+  # =========================== GET ORIENTATION ================================
   if   (xy[0] == 9):
+    # Hit on lower field border
     surr = np.array([-1])
   elif (xy[0] == 0):
+    # Hit on upper field border
     surr = np.array([1])
   else:
     surr = np.array([-1, 1])
     
-  if ( (shipMap[xy[0] + surr, xy[1], selPl]%2).any() ):
+  if ( (shipMap[xy[0] + surr, xy[1]]%2).any() ):
+    # Ship is placed vertically
     itVec = [1, 0]
     xOrient = xy[0]
   else:
+    # Ship is placed horizontally
     itVec = [0, 1]
     xOrient = xy[1]
     
+  # ======================== GET SHIP SURROUNDING ==============================
   selSurr  = np.arange(max(-5, -xOrient), min(5, 9-xOrient)+1, 1, dtype='int')
   fixPos = min(5, xOrient)
-  surr = shipMap[xy[0] + itVec[0]*selSurr, xy[1] + itVec[1]*selSurr, selPl]
+  surr = shipMap[xy[0] + itVec[0]*selSurr, xy[1] + itVec[1]*selSurr]
   waterArea  = (surr%2==0).nonzero()[0]
   intactShip = (surr==1).nonzero()[0]
   dest = [False, False]
   shipStartEnd = [-1, -1]
   
-  # Check positive half of surrounding
+  # ========================= CHECK POSITIVE SIDE ==============================
   arrWater = waterArea[waterArea > fixPos]
   arrShip  = intactShip[intactShip > fixPos]
-  if   ( len(arrShip)  == 0 ):                          # No intact ship in positive half       --> pos Half destroyed
-    dest[0] = True
+  if   ( len(arrShip)  == 0 ):                    # No intact ship in positive half       --> pos Half destroyed
+    dest[1] = True
+  elif ( len(arrWater) == 0 ):                    # No water in vicinity, but intact ship --> Ship touches border and is not destroyed
+    dest[1] = False
+  elif ( arrWater[0] < arrShip[0] ):              # Both classes appear                   --> Compare what comes first
+    dest[1] = True
+  else:                                           # Only for readabilty
+    dest[1] = False
+    
+  if ( len(arrWater) != 0):
+    shipStartEnd[1] = xOrient + (arrWater[0]-fixPos)-1
+  else:
     shipStartEnd[1] = 9
-  elif ( len(arrWater) == 0 ): dest[0] = False                        # No water in vicinity, but intact ship --> Ship touches border and is not destroyed
-  elif ( arrWater[0] < arrShip[0] ):                                  # Both classes appear                   --> Compare what comes first
-    dest[0] = True
+    
 
-  # Check negative half of surrounding
+  # ========================= CHECK NEGATIVE SIDE ==============================
   arrWater = waterArea[waterArea < fixPos]
   arrShip  = intactShip[intactShip < fixPos]
-  if   ( len(arrShip)  == 0 ): dest[1] = True                         # No intact ship in negative half       --> neg Half destroyed
-  elif ( len(arrWater) == 0 ): dest[1] = False                        # No water in vicinity, but intact ship --> Ship touches border and is not destroyed
-  else:                        dest[1] = arrWater[-1] > arrShip[-1]   # Both classes appear                   --> Compare what comes first
+  if   ( len(arrShip)  == 0 ):                    # No intact ship in negative half       --> neg Half destroyed
+    dest[0] = True
+  elif ( len(arrWater) == 0 ):                    # No water in vicinity, but intact ship --> Ship touches border and is not destroyed
+    dest[0] = False
+  elif (arrWater[-1] > arrShip[-1]):              # Both classes appear                   --> Compare what comes first
+    dest[0] = True
+  else:                                           # Only for readabilty
+    dest[0] = False
+    
+  if ( len(arrWater) != 0):
+    shipStartEnd[0] = xOrient + (arrWater[-1]-fixPos)+1
+  else:
+    shipStartEnd[0] = 0
 
+  # =========================== MARK BOMBED AREA ===============================
   # If ship is destroyed set bombed area around ship
-  # if (dest[0] and dest[1]):
+  if (dest[0] and dest[1]):
+    if (itVec[0] == 1):
+      xExt = [max(0, shipStartEnd[0]-1), min(9, shipStartEnd[1]+1)]
+      yExt = [max(0, xy[1]-1), min(9, xy[1]+1)]
+    else:
+      xExt = [max(0, xy[0]-1), min(9, xy[0]+1)]      
+      yExt = [max(0, shipStartEnd[0]-1), min(9, shipStartEnd[1]+1)]
+    shipMap[xExt[0]:xExt[1]+1, yExt[0]:yExt[1]+1] = (
+           shipMap[xExt[0]:xExt[1]+1, yExt[0]:yExt[1]+1]%2 + 2)
+    if (debug):
+      print("Ship destroyed - excluded area ({}-{}, {}-{})"
+            .format(xExt[0], xExt[1], yExt[0], yExt[1]))
+      print("                 Startend: {}, {}"
+            .format(shipStartEnd[0], shipStartEnd[1]))
+    return 1
+  else:
+    return 0
+    
+    
+    
+    
+def parseCommand(buff, cl):
+  """Parse and handle client commands
+  Input:
+    buff:      String containing the command to be handled
+    cl:        Socket for client connection"""
+  # ====================== SPLIT COMMAND IF NECESSARY ==========================
+  if ((buff[0] == 'B') or (buff[0] == 'M')):
+    # Position Command
+    splittedBuff = buff.split(',')
+    try:
+      xy = [int(splittedBuff[1]), int(splittedBuff[2])]
+    except:
+      invalid = True
+    else: 
+      invalid = False
+    if (max(xy)>9 or min(xy)<0 or invalid):
+      cl.send("I")
+      if (sent == 0):
+        raise "ERROR> Sending invalid flag"
+      return 'I', -1
+      
+  # =========================== HANDLE AND REPLY ===============================
+  if   buff[0] == 'B':
+    state = shipMap[xy[0], xy[1]]%2
+    shipMap[xy[0], xy[1]] = state+2
+    if (state):
+      dest  = checkDestroyed(shipMap, xy)
+    else:
+      dest = 0
+    sent = cl.send("R, {}, {}".format(state, dest))   # Returns R, then if it was a hit and if the ship was destroyed
+    if (sent == 0):
+      raise "ERROR> Sending B result failed"
+    return  'B', [xy[0], xy[1], state, dest]
+    
+  elif buff[0] == 'M':
+    state = max(1, shipMap[xy[0], xy[1]])
+    if (state == 1): state = 0              # This way the bot can not derive the real state via runtime analysis
+    sent = cl.send("R, {}".format(state))
+    if (sent == 0):
+      raise "ERROR> Sending M result failed"
+    return  'M', [xy[0], xy[1], state]
 
-  return (dest[0] and dest[1])
+  else:
+    # Invalid command
+    cl.send("I")
+    if (sent == 0):
+      raise "ERROR> Sending invalid flag"
+    return 'I', -1
 ################################################################################
 
 
+
+
+
+################################################################################
+###                           MAIN LOOP                                      ###
+################################################################################
+
+# ========================= OPEN CONNECTION ====================================
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
+s.listen(1)                 # Now wait for 1 client connection.
+cls, adds = s.accept()      # Establish connection with client.
 
-s.listen(2)                 # Now wait for client connection.
+print 'Got connection from', adds
+sent = saveSend(cls, 'Y - Thank you for connecting')
+if (sent == 0):
+  # Handshake error
+  raise "ERROR> Handshake not successfull"
 
-conBots = 0
-cls  = [0,0]
-adds = [0,0]
-while conBots < 1:
-  cls[conBots], adds[conBots] = s.accept()     # Establish connection with client.
-  print 'Got connection from', adds[conBots]
-  cls[conBots].send('Thank you for connecting - You are Player {}'.format(conBots+1))
-  conBots += 1
-
-cls[0].send("All set up, lets go!")
-# cls[1].send("All set up, lets go!")
-# At this point we have two participants connected
-
-# Place ships randomly
-shipMap = np.zeros((10, 10, 2))
-setShipsRandom(shipMap, 0)
-setShipsRandom(shipMap, 1)
-print("SHIPS SET UP")
-
-pp.matshow(shipMap[:,:,0])
-pp.show()
-pp.matshow(shipMap[:,:,1])
-pp.show()  
+# =========================== PLACE SHIPS ======================================
+shipMap = np.zeros((10, 10), dtype=int)
+setShipsRandom(shipMap)
+print("All ships set")
 
 
-# Handle client commands
-def parseCommand(buff, cl, pl):
-  if   buff[0] == 'B':
-    splittedBuff = buff.split(',')
-    xy = [int(splittedBuff[1]), int(splittedBuff[2])]
-    if (max(xy)>9 or min(xy)<0):
-      cl.send("I")
-      return 'I'
-    else:
-      state = shipMap[xy[0], xy[1], (pl+1)%2]%2
-      shipMap[xy[0], xy[1], (pl+1)%2] = state+2
-      if (state):
-        dest  = checkDestroyed(shipMap, pl, xy)
-      else:
-        dest = 0
-
-      cl.send("R, {}, {}".format(state, dest))
-      
-      if (dest == 1):
-        pp.matshow(shipMap[:,:,(pl+1)%2])
-        pp.plot(xy[1], xy[0], 'ko')
-        pp.show()
-        
-      return  'B'
-  elif buff[0] == 'M':
-    # Parse map request
-    return  'M'
-  else:
-    # Invalid command
-    cl.send("I")
-    return 'I'
+if (visu):
+  pp.imshow(shipMap, interpolation='None', vmin=0, vmax=3)
+  pp.xlim([-0.5, 9.5])
+  pp.ylim([-0.5, 9.5])
+  pp.show(0)
 
 
 for steps in range(1000):
-  cls[0].send("T")
-  for i in range(1000):
-    buff = cls[0].recv(1024)
-    act = parseCommand(buff, cls[0], 0)
-    if (act == 'B'): break
-  if ( min(shipMap[shipMap[:,:,1]%2>0,1]) == 3 ):
+  saveSend(cls, "T")
+  buff = cls.recv(1024)
+  if (buff == 'Y'):
+    print("Round {}".format(steps))   
+    for i in range(1000):
+      buff = cls.recv(1024)
+      if (debug):
+        print("Received command: " + buff)
+      act, result = parseCommand(buff, cls)
+      
+      if (act == 'B') and (visu):
+        pp.imshow(shipMap, interpolation='None', vmin=0, vmax=3)
+        pp.plot(result[1], result[0], 'ko')
+        pp.xlim([-0.5, 9.5])
+        pp.ylim([-0.5, 9.5])
+        pp.show()
+      if (act == 'B'): 
+        break
+  else:
+    if (debug):
+      print("Received unexpected command")
+    saveSend(cls, "N")
+  
+  # Check if all ships are destroyed!
+  if ( min(shipMap[shipMap%2>0]) == 3 ):
     break
   
-cls[0].send("EOG")
-# cls[1].send("EOG")
-
-cls[0].close()                # Close the connection
-# cls[1].close()                # Close the connection
+cls.send("EOG")
+print("Finished after {} steps".format(steps))
+cls.close()                # Close the connection
